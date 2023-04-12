8aa4278e04f3a3cc85741415c8746ef87d1b10e1
germandefrancesco <germandefrancesco@hotmail.com>
2023-04-12 16:05:24 -0300

    Move QrReader to its own component

--- BlazorQRCodeReaderDemo.ServerSide/Program.cs
+++ BlazorQRCodeReaderDemo.ServerSide/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices(config =>
 {
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
     config.SnackbarConfiguration.PreventDuplicates = false;
 });
 
